15d62a80ef8354d2b3f1e85e05a752013ab7fc6a
KncskBenedek <koncsikbenedek@gmail.com>
2022-03-20 12:34:45 +0100

    Javascript léptető

---
+++ lepteto.js
@@ -0,0 +1,58 @@
+window.addEventListener("load", init, false);
+var kepekObj = [
+    {
+        ut: "kepek/_DSC7025.jpg",
+        leiras: "Bolond állatok",
+    },
+    {
+        ut: "kepek/_DSC7365.jpg",
+        leiras: "virágzó út",
+    },
+    {
+        ut: "kepek/_DSC7371_1.jpg",
+        leiras: "Nap lemente",
+    },
+    {
+        ut: "kepek/_DSC7444.jpg",
+        leiras: "Tájkép",
+    },
+    {
+        ut: "kepek/_DSC7515.jpg",
+        leiras: "Akác sor",
+    },
+];
+var hely = 0;
+function init() {
+    leiras();
+    kep();
+
+}
+
+function ID(elem) {
+    return document.getElementById(elem);
+}
+
+function leiras() {
+    ID("leiras").innerHTML = kepekObj[hely].leiras;
+}
+function kep() {
+    ID("nagykep").src = kepekObj[hely].ut;
+}
+function bal() {
+    if (hely === 0) {
+        hely = kepekObj.length - 1;
+    }else{
+        hely-= 1;
+    }
+    leiras();
+    kep();
+}
+function jobb(){
+    if(jobb === kepekObj.length-1){
+        hely = 0;
+    }else{
+        hely+=1;
+    }
+    leiras();
+    kep();
+}
