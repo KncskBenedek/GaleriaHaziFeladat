b2e87972fdd6353f22c8c11a4585926240e4165a
KncskBenedek <koncsikbenedek@gmail.com>
2022-03-20 13:59:37 +0100

    képre kattintva változik

--- lepteto.js
+++ lepteto.js
@@ -3,22 +3,27 @@ var kepekObj = [
     {
         ut: "kepek/_DSC7025.jpg",
         leiras: "Bolond állatok",
+        id: "kep1",
     },
     {
         ut: "kepek/_DSC7365.jpg",
         leiras: "virágzó út",
+        id: "kep2",
     },
     {
         ut: "kepek/_DSC7371_1.jpg",
         leiras: "Nap lemente",
+        id: "kep3",
     },
     {
         ut: "kepek/_DSC7444.jpg",
         leiras: "Tájkép",
+        id: "kep4",
     },
     {
         ut: "kepek/_DSC7515.jpg",
         leiras: "Akác sor",
+        id: "kep5",
     },
 ];
 var hely = 0;
@@ -40,18 +45,29 @@ function kep() {
 function bal() {
     if (hely === 0) {
         hely = kepekObj.length - 1;
-    }else{
-        hely-= 1;
+    } else {
+        hely -= 1;
     }
     leiras();
     kep();
 }
-function jobb(){
-    if(hely === kepekObj.length-1){
+function jobb() {
+    if (hely === kepekObj.length - 1) {
         hely = 0;
-    }else{
-        hely+=1;
+    } else {
+        hely += 1;
     }
+    leiras();
+    kep();
+}
+
+function kepreKatt() {
+    var kepId = event.target.id;
+    var i = 0;
+    while(!(kepId === kepekObj[i].id)){
+        i++;
+    }
+    hely = i;
     leiras();
     kep();
 }
